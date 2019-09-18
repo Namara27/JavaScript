747982d5ac7044e59340d39ab525a1fec8217b3e
Namara27 <namara.kerkenaar@student.rocvf.nl>
2019-09-18 15:34:56 +0200

    opdracht 3 toegevoegd

---
+++ Opdracht3/opdracht3_1.js
@@ -0,0 +1,16 @@
+var pictures = document.getElementsByTagName("img");
+var random = 0;
+var randomNumbers = [];
+
+while (randomNumbers.length < 9) {
+    random = Math.floor(Math.random() * 9 + 1);
+    if (randomNumbers.lastIndexOf(random) == -1) {
+        randomNumbers.push(random);
+    }
+    
+}
+random = 0;
+for (var picture in pictures) {
+    pictures[picture].src = "img/disney" + randomNumbers[random] + ".jpg";
+    random++
+}
