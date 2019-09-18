5edc6b255d5a95b1bafb9f9021400425c801dcc4
Namara27 <namara.kerkenaar@student.rocvf.nl>
2019-09-18 09:38:51 +0200

    eerste commit

---
+++ Opdracht2/opdracht2_1.js
@@ -0,0 +1,4 @@
+document.getElementById("paginatitel").innerHTML = "Dit is een paginatitel";
+document.getElementById("subkop").innerHTML = "Dit is een subkop";
+document.getElementById("banaan1").src = "img/banaan1.jpg";
+document.getElementById("banaan2").src = "img/banaan2.jpg";
---
+++ Opdracht2/opdracht2_2.js
@@ -0,0 +1,15 @@
+bl1.style.backgroundColor = "aqua";
+bl1.style.textAlign = "center";
+bl1.style.paddingTop = "25px";
+
+bl2.style.backgroundColor = "green";
+bl2.style.textAlign = "center";
+bl2.style.marginTop = "20px";
+bl2.style.paddingTop = "5px";
+
+bl3.style.marginTop = "50px";
+bl3.style.paddingTop = "5px";
+bl3.style.textAlign = "right";
+
+bl4.style.backgroundColor = "red";
+bl4.style.paddingTop = "5px";
